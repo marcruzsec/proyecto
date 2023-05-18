306bb6bf88d069436fb3aa938ec0ed742d05646f
marcruzsec <133927883+marcruzsec@users.noreply.github.com>
2023-05-17 19:43:55 -0500

    Add files via upload

---
+++ Taller1/vista/layouts/footer.php
@@ -0,0 +1,11 @@
+       <!-- /contenido-->
+
+       <h1 class="text-center">LA SEGURIDAD ES NUESTRA RESPONSABILIDAD</h1>
+       <h1 class="text-center">FORMAMOS CULTURA EN SEGURIDAD</h1>
+
+
+    
+       </div> 
+
+</body>
+</html>
